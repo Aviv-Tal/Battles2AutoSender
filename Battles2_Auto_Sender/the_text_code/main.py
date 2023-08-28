7844820601b1579bff3c24b04d8ab0c222746587
Aviv-Tal <105798911+Aviv-Tal@users.noreply.github.com>
2023-08-28 12:57:55 +0300

    tried to fix something

--- Battles2_Auto_Sender/the_text_code/main.py
+++ Battles2_Auto_Sender/the_text_code/main.py
@@ -1,16 +1,15 @@
-from OpenBattles import openBattles
-from KeyboardAutoSender import keyboardAutoSend
-from MouseAutoSender import mouseAutoSend, startGame, configPositions
-import Utility as util
-from mouse import wait
+try:
+    from OpenBattles import openBattles
+    from KeyboardAutoSender import keyboardAutoSend
+    import Utility as util
+    from mouse import wait
 
-if __name__ == "__main__":
-    try:
+    if __name__ == "__main__":
         file_content = open(util.path_to_variables, "r").readlines()
         legal_keys = util.getSendHotKeys(file_content)
-                
+                    
         openBattles(file_content)
         keyboardAutoSend(legal_keys)
             
-    except Exception as e:
-        input(f"Exeption {e}\npress enter to exit program")
+except Exception as e:
+    input(f"Exeption {e}\npress enter to exit program")
