1d7ab97ddfa798e39b41d1a75d54b851b64e9033
Aviv-Tal <105798911+Aviv-Tal@users.noreply.github.com>
2023-08-31 01:16:36 +0300

    tried using the built in alt function to auto send the bloon
    should make the program faster, for some reason doesnt really work with the .hotkey() function

--- Battles2_Auto_Sender/the_text_code/KeyboardAutoSender.py
+++ Battles2_Auto_Sender/the_text_code/KeyboardAutoSender.py
@@ -3,22 +3,27 @@ from mouse import is_pressed as pressed, wait
 from time import sleep
 from pyautogui import hotkey
 import Utility as util
+import pyautogui
 
 def keyboardAutoSend(legal_keys: list):
+    last_key = "l"
     while True:
         key = read_key()
+        if key == "z":
+            pyautogui.press(last_key)
+            continue
         if key not in legal_keys:
             continue
-        while True:
-            if pressed(button="right"):
-                if util.getActiveWindow() == "Bloons TD Battles 2":
-                    break
-            if util.getActiveWindow() == "Bloons TD Battles 2":
-                press(key)
-                sleep(0.05)
-        release(key)
+        pyautogui.press(last_key)
+        sleep(0.1)
+        pyautogui.hotkey(key, "alt") #TODO - make this work
+        last_key = key
+        
         
 if __name__ == "__main__":
-    f = open(util.path_to_variables, "r")
-    legal_keys = util.getSendHotKeys(f.readlines())
-    keyboardAutoSend(legal_keys)
+    try:
+        f = open(util.path_to_variables, "r")
+        legal_keys = util.getSendHotKeys(f.readlines())
+        keyboardAutoSend(legal_keys)
+    except Exception as e:
+        input(f"Exception {e}")
