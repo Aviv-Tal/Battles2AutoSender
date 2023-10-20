935c41363e5ecf4f503d934c36949814aa2243d6
Aviv-Tal <105798911+Aviv-Tal@users.noreply.github.com>
2023-10-20 16:58:34 +0300

    mistakes were made

--- Battles2_Auto_Sender/the_text_code/main.py
+++ Battles2_Auto_Sender/the_text_code/main.py
@@ -3,13 +3,29 @@ try:
     from KeyboardAutoSender import keyboardAutoSend
     import Utility as util
     from mouse import wait
-
-    if __name__ == "__main__":
+    from threading import Thread
+    from time import sleep
+    
+    def main_program() -> None:
         file_content = open(util.path_to_variables, "r").readlines()
         legal_keys = util.getSendHotKeys(file_content)
         
         openBattles(file_content)
         keyboardAutoSend(legal_keys)
+    
+    def auto_shutdown() -> None:
+        sleep(10)
+        while util.isBattlesRunning():
+            pass
+        exit()
+
+    if __name__ == "__main__":
+        program_thread = Thread(target=main_program)
+        auto_shutdown_thread = Thread(target=auto_shutdown)
+        
+        program_thread.start()
+        auto_shutdown_thread.start()
+        
             
 except Exception as e:
-    input(f"Exeption {e}\npress enter to exit program")
+    print(f"Exeption {e}\ndeal with it")
